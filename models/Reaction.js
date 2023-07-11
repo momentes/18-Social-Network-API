50c1a0a8361346e27748c0901fa49c1d50b17cfe
Bryan S <72003881+momentes@users.noreply.github.com>
2023-07-11 17:18:32 +1000

    model update

--- models/Reaction.js
+++ models/Reaction.js
@@ -2,31 +2,34 @@ const { Schema, Types } = require('mongoose');
 
 const reactionSchema = new Schema(
   {
-    reactionId: {
-      type: Schema.Types.ObjectId,
-      default: () => new Types.ObjectId(),
-    },
-    reactionBody: {
-      type: String,
-      required: true,
-      maxlength: 280,
-      minlength: 4,
-    },
-    username: {
-      type: String,
-      required: true,
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-    },
+      reactionID: {
+          type: Schema.Types.ObjectId,
+          default: () => new Types.ObjectId,
+      },
+      reactionBody: {
+          type: String,
+          required: true,
+          maxlength: 280
+      },
+      username: {
+          type: String,
+          required: true
+      },
+      createdAt: {
+          type: Date,
+          default: Date.now,
+          get: (date) => {
+              return date.toLocaleString();
+          }
+      }
   },
   {
-    toJSON: {
-      getters: true,
-    },
-    id: false,
+      toJSON: {
+          getters: true,
+      },
+      id: false,
   }
 );
 
 module.exports = reactionSchema;
+
